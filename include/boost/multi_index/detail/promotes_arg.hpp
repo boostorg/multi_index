df022fa6592fec4d461dd05282e0290b91019adc
joaquintides <joaquin@tid.es>
2017-01-08 13:24:13 +0100

    shut down promotes_1st_arg/promotes_2nd_arg for MSVC 7.1

--- include/boost/multi_index/detail/promotes_arg.hpp
+++ include/boost/multi_index/detail/promotes_arg.hpp
@@ -1,4 +1,4 @@
-/* Copyright 2003-2016 Joaquin M Lopez Munoz.
+/* Copyright 2003-2017 Joaquin M Lopez Munoz.
  * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
@@ -14,16 +14,40 @@
 #endif
 
 #include <boost/config.hpp> /* keep it first to prevent nasty warns in MSVC */
-#include <boost/mpl/and.hpp>
-#include <boost/mpl/bool.hpp>
-#include <boost/mpl/not.hpp>
-#include <boost/multi_index/detail/is_transparent.hpp>
-#include <boost/type_traits/is_convertible.hpp>
+#include <boost/detail/workaround.hpp>
 
 /* Metafunctions to check if f(arg1,arg2) promotes either arg1 to the type of
  * arg2 or viceversa. By default, (i.e. if it cannot be determined), no
  * promotion is assumed.
  */
+
+#if BOOST_WORKAROUND(BOOST_MSVC,<1400)
+
+namespace boost{
+
+namespace multi_index{
+
+namespace detail{
+
+template<typename F,typename Arg1,typename Arg2>
+struct promotes_1st_arg:mpl::false_{};
+
+template<typename F,typename Arg1,typename Arg2>
+struct promotes_2nd_arg:mpl::false_{};
+
+} /* namespace multi_index::detail */
+
+} /* namespace multi_index */
+
+} /* namespace boost */
+
+#else
+
+#include <boost/mpl/and.hpp>
+#include <boost/mpl/bool.hpp>
+#include <boost/mpl/not.hpp>
+#include <boost/multi_index/detail/is_transparent.hpp>
+#include <boost/type_traits/is_convertible.hpp>
 
 namespace boost{
 
@@ -55,4 +79,5 @@ struct promotes_2nd_arg:
 
 } /* namespace boost */
 
+#endif
 #endif
