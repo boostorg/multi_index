2f21af1751c114ae0ae92d0fad69f273550c6a70
joaquintides <joaquin@tid.es>
2018-01-01 11:36:34 +0100

    documented previous commit

--- include/boost/multi_index_container.hpp
+++ include/boost/multi_index_container.hpp
@@ -1,6 +1,6 @@
 /* Multiply indexed container.
  *
- * Copyright 2003-2017 Joaquin M Lopez Munoz.
+ * Copyright 2003-2018 Joaquin M Lopez Munoz.
  * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
