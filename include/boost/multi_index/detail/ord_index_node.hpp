55fb849c2ca9a194071545d2226f8bb552d76858
Laurent Stacul <laurent.stacul@amadeus.com>
2019-02-28 16:02:41 +0000

    Fix gcc -Wdeprecated-copy

--- include/boost/multi_index/detail/ord_index_node.hpp
+++ include/boost/multi_index/detail/ord_index_node.hpp
@@ -143,6 +143,8 @@ struct ordered_index_node_compressed_base
   struct color_ref
   {
     color_ref(uintptr_type* r_):r(r_){}
+
+    color_ref(color_ref const& x):r(x.r){}
     
     operator ordered_index_color()const
     {
@@ -168,6 +170,8 @@ struct ordered_index_node_compressed_base
   struct parent_ref
   {
     parent_ref(uintptr_type* r_):r(r_){}
+
+    parent_ref(parent_ref const& x):r(x.r){}
     
     operator pointer()const
     {
