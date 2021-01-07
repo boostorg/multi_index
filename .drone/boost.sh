bd2f183055ba75a2eb0f06b4970213eb7eb0ba45
sdarwin <samuel.d.darwin@gmail.com>
2021-01-07 19:14:31 +0000

    remove TRAVIS_OS_NAME from drone script [ci skip]

--- .drone/boost.sh
+++ .drone/boost.sh
@@ -2,11 +2,10 @@
 
 set -ex
 export TRAVIS_BUILD_DIR=$(pwd)
+export DRONE_BUILD_DIR=$(pwd)
 export TRAVIS_BRANCH=$DRONE_BRANCH
-export TRAVIS_OS_NAME=${DRONE_JOB_OS_NAME:-linux}
 export VCS_COMMIT_ID=$DRONE_COMMIT
 export GIT_COMMIT=$DRONE_COMMIT
-export DRONE_CURRENT_BUILD_DIR=$(pwd)
 export PATH=~/.local/bin:/usr/local/bin:$PATH
 
 echo '==================================> BEFORE_INSTALL'
@@ -27,7 +26,7 @@ cp -r $TRAVIS_BUILD_DIR/* libs/multi_index
 
 echo '==================================> BEFORE_SCRIPT'
 
-. $DRONE_CURRENT_BUILD_DIR/.drone/before-script.sh
+. $DRONE_BUILD_DIR/.drone/before-script.sh
 
 echo '==================================> SCRIPT'
 
@@ -36,4 +35,4 @@ echo "using $TOOLSET : : $COMPILER ;" > ~/user-config.jam
 
 echo '==================================> AFTER_SUCCESS'
 
-. $DRONE_CURRENT_BUILD_DIR/.drone/after-success.sh
+. $DRONE_BUILD_DIR/.drone/after-success.sh
