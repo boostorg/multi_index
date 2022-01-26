b64f1f14880b3fee684526ef9f06865cec8f8712
DamianSawicki <86234282+DamianSawicki@users.noreply.github.com>
2022-01-26 14:20:44 +0100

    Added test for new count(x) and count(x,comp) in ranked_index
    Both, the existing implementation of count from ordered_index and
    the new implementation for ranked_index are compared with the
    common sense. Positive results of this test show in particular that
    the numbers produced by the new implementation are consistent with
    those from the existing implementation and hence correct.

--- test/test_all_main.cpp
+++ test/test_all_main.cpp
@@ -16,6 +16,7 @@
 #include "test_composite_key.hpp"
 #include "test_conv_iterators.hpp"
 #include "test_copy_assignment.hpp"
+#include "test_count.hpp"
 #include "test_hash_ops.hpp"
 #include "test_iterators.hpp"
 #include "test_key.hpp"
@@ -44,6 +45,7 @@ int main()
   test_composite_key();
   test_conv_iterators();
   test_copy_assignment();
+  test_count();
   test_hash_ops();
   test_iterators();
   test_key();
---
+++ test/test_count_main.cpp
@@ -0,0 +1,19 @@
+/* Boost.MultiIndex count test.
+ *
+ * Copyright 2003-2008 Joaquin M Lopez Munoz.
+ * Copyright 2022 Damian Sawicki.
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * See http://www.boost.org/libs/multi_index for library home page.
+ */
+
+#include <boost/detail/lightweight_test.hpp>
+#include "test_count.hpp"
+
+int main()
+{
+  test_count();
+  return boost::report_errors();
+}
