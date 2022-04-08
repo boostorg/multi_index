fba66b05384c9f77418304d352aba86e99b854e4
joaquintides <joaquin@tid.es>
2022-04-08 17:13:04 +0200

    updated dates and release notes

--- test/test_rearrange.cpp
+++ test/test_rearrange.cpp
@@ -1,6 +1,6 @@
 /* Boost.MultiIndex test for rearrange operations.
  *
- * Copyright 2003-2013 Joaquin M Lopez Munoz.
+ * Copyright 2003-2022 Joaquin M Lopez Munoz.
  * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
