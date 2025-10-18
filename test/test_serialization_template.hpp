9df4625d94f31707e1dc2de960c4bb12ab1546b2
joaquintides <joaquin@tid.es>
2025-10-18 20:42:30 +0200

    leftover MPL->Mp11 change

--- test/test_serialization_template.hpp
+++ test/test_serialization_template.hpp
@@ -1,6 +1,6 @@
 /* Boost.MultiIndex serialization tests template.
  *
- * Copyright 2003-2013 Joaquin M Lopez Munoz.
+ * Copyright 2003-2025 Joaquin M Lopez Munoz.
  * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
@@ -12,7 +12,7 @@
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/detail/lightweight_test.hpp>
-#include <boost/mpl/size.hpp>
+#include <boost/mp11/list.hpp>
 #include "pre_multi_index.hpp"
 #include <boost/multi_index_container.hpp>
 #include <string>
@@ -49,8 +49,8 @@ bool all_indices_equal(
   const MultiIndexContainer& m1,const MultiIndexContainer& m2)
 {
   BOOST_STATIC_CONSTANT(int,
-    N=boost::mpl::size<
-    BOOST_DEDUCED_TYPENAME MultiIndexContainer::index_type_list>::type::value);
+    N=boost::mp11::mp_size<
+    BOOST_DEDUCED_TYPENAME MultiIndexContainer::index_type_list>::value);
 
   return all_indices_equal_helper<N-1>::compare(m1,m2);
 }
