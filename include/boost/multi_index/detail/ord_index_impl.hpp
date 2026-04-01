52ccca406fea7a4bb82ec53b8bad622585d14c63
joaquintides <joaquin@tid.es>
2026-04-01 10:01:10 +0200

    fixed assumption

--- include/boost/multi_index/detail/ord_index_impl.hpp
+++ include/boost/multi_index/detail/ord_index_impl.hpp
@@ -1220,7 +1220,7 @@ private:
       else return link_point(k,inf,ordered_unique_tag());
     } 
     else if(position==header()){ 
-      BOOST_MULTI_INDEX_ASSUME(rightmost()!=pointer(0));
+      BOOST_MULTI_INDEX_ASSUME(rightmost()!=0);
       if(comp_(key(rightmost()->value()),k)){
         inf.side=to_right;
         inf.pos=rightmost()->impl();
