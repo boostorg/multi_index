14afe8a6f912a8c9511c782429794bf4915f36ec
joaquintides <joaquin@tid.es>
2026-06-27 12:19:05 +0200

    added hashed index local iterators to the test

--- test/test_mmap.cpp
+++ test/test_mmap.cpp
@@ -45,6 +45,7 @@ void test_mmap()
   >;
   using iterator0=nth_index<container,0>::type::iterator;
   using iterator1=nth_index<container,1>::type::iterator;
+  using local_iterator1=nth_index<container,1>::type::local_iterator;
   using iterator2=nth_index<container,2>::type::iterator;
   using iterator3=nth_index<container,3>::type::iterator;
   using iterator4=nth_index<container,4>::type::iterator;
@@ -64,6 +65,7 @@ void test_mmap()
   for(int i=0;i<100;++i)c1.insert(i);
   auto v0=**pseg1->construct<iterator0>("it0")(c1.get<0>().begin());
   auto v1=**pseg1->construct<iterator1>("it1")(c1.get<1>().begin());
+  auto lv1=**pseg1->construct<local_iterator1>("lit1")(c1.get<1>().begin(0));
   auto v2=**pseg1->construct<iterator2>("it2")(c1.get<2>().begin());
   auto v3=**pseg1->construct<iterator3>("it3")(c1.get<3>().begin());
   auto v4=**pseg1->construct<iterator4>("it4")(c1.get<4>().begin());
@@ -73,6 +75,7 @@ void test_mmap()
   container& c2=*seg2.find<container>(container_name).first;
   auto it0=*seg2.find<iterator0>("it0").first;
   auto it1=*seg2.find<iterator1>("it1").first;
+  auto lit1=*seg2.find<local_iterator1>("lit1").first;
   auto it2=*seg2.find<iterator2>("it2").first;
   auto it3=*seg2.find<iterator3>("it3").first;
   auto it4=*seg2.find<iterator4>("it4").first;
@@ -80,6 +83,9 @@ void test_mmap()
   BOOST_TEST_EQ(std::distance(it0,c2.get<0>().end()),c2.size());
   BOOST_TEST_EQ(*it1,v1);
   BOOST_TEST_EQ(std::distance(it1,c2.get<1>().end()),c2.size());
+  BOOST_TEST_EQ(*lit1,lv1);
+  BOOST_TEST_EQ(
+    std::distance(lit1,c2.get<1>().end(0)),c2.get<1>().bucket_size(0));
   BOOST_TEST_EQ(*it2,v2);
   BOOST_TEST_EQ(std::distance(it2,c2.get<2>().end()),c2.size());
   BOOST_TEST_EQ(*it3,v3);
